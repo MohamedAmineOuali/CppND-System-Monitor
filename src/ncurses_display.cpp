05658cb1ebad455edc41fc2e841050e1a7b68b11
Mohamed Amine Ouali <M.Amine.wali@gmail.com>
2019-12-15 22:19:06 +0100

    Init commit
    Support for Curses6

--- src/ncurses_display.cpp
+++ src/ncurses_display.cpp
@@ -78,7 +78,7 @@ void NCursesDisplay::DisplayProcesses(std::vector<Process>& processes,
     mvwprintw(window, row, time_column,
               Format::ElapsedTime(processes[i].UpTime()).c_str());
     mvwprintw(window, row, command_column,
-              processes[i].Command().substr(0, window->_maxx - 46).c_str());
+              processes[i].Command().substr(0, getmaxx(window) - 46).c_str());
   }
 }
 
@@ -91,7 +91,7 @@ void NCursesDisplay::Display(System& system, int n) {
   int x_max{getmaxx(stdscr)};
   WINDOW* system_window = newwin(9, x_max - 1, 0, 0);
   WINDOW* process_window =
-      newwin(3 + n, x_max - 1, system_window->_maxy + 1, 0);
+      newwin(3 + n, x_max - 1, getmaxy(system_window), 0);
 
   while (1) {
     init_pair(1, COLOR_BLUE, COLOR_BLACK);
