6e8f78df42aeca2954f9c3f1abe473f9abcc1905
Mohamed Amine Ouali <M.Amine.wali@gmail.com>
2019-12-16 15:15:48 +0100

    Helper function
    y

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,23 @@
 #include <string>
+#include <sstream>
+#include <iomanip>
 
 #include "format.h"
 
 using std::string;
 
-// TODO: Complete this helper function
+
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+    long minute=seconds/60;
+    long hour=minute/60;
+    minute%=60;
+    seconds%=60;
+    std::ostringstream out;
+    out<<std::setw(2)<<std::setfill('0')<<hour<<":"<<
+        std::setw(2)<<std::setfill('0')<<minute<<":"<<
+        std::setw(2)<<std::setfill('0')<<seconds;
+    return out.str();
+}
